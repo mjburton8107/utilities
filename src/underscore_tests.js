ced892dbcfca2e67de4e85c0439637accbb8c326
MikeB <burton.mj@gmail.com>
2017-01-24 20:48:54 -0700

    mid progress

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,11 +16,23 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if(!n){
+      return array[0];
+    } else {
+      return array.slice(0,n);
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(!n){
+      return array[array.length-1];
+    } else if (n > array.length){
+      return array;
+    } else {
+      return array.splice((array.length-n), array.length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
